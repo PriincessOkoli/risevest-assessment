ac71637eaf0ccd1fc9730e8988c5468401a285c4
okoli Chimarume <juskie115@gmail.com>
2025-02-20 10:40:54 +0100

    edit

--- src/components/OurOffers.tsx
+++ src/components/OurOffers.tsx
@@ -20,7 +20,7 @@ function OurOffers() {
         >
           <div className="relative h-full">
             <div
-              className={`section px-6 py-5 lg:py-0 mx-4 xl:mx-24 lg:mx-8  mb-14 lg:mb-0 lg:mt-10 flex justify-between items-center flex-wrap lg:flex-nowrap mx-auto rounded-[2rem] ${offer.bg}`}
+              className={`section px-6 py-5 lg:py-0 xl:mx-24 lg:mx-8  mb-14 lg:mb-0 lg:mt-10 flex justify-between items-center flex-wrap lg:flex-nowrap mx-auto rounded-[2rem] ${offer.bg}`}
             >
               <div className="py-3 lg:p-4 lg:pl-4 lg:w-[33.6rem]">
                 <h2 className="text-2xl xl:text-5xl text-dark font-semibold">
